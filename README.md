8e1ded2892a85fa11b79cd6208d2019c6f829deb
Ben MacGillivray <benmacgillivray@Bens-MBP-2.cg.shawcable.net>
2021-02-16 17:04:50 -0700

    hopefully final commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# GroceryCo
+The program takes the name of the catalogue file as a command line argument but the file can be modified while the program is running and the next checkout will recognize those changes. The "basket" of items being checked out is a file provided by the user while the program is running. 
+
+I ran the program with the following commands:
+
+1. mcs kiosk.cs foodItem.cs 
+2. mono kiosk.exe catalogue.txt
+
+As you can see in the commit history, I initially had a slightly different implementation for this problem. After some thought I decided to create a class to represent the items in the catalogue rather than using a dictionary. While this isn't strictly necessary to meet the requirements of the assignment, and it may be marginally more expensive, it hopefully makes the code easier to expand in the future.
+
+
+The expected format of the catalogue file is: 
+
+item,price
+item,price,saleprice  if applicable
+
+Apologies in advance if any of the syntax is not up to standard, this is my first time writing c#.
+
+Thanks for the opportunity to interview!
+
--- kiosk.cs
+++ kiosk.cs
@@ -1,85 +1,103 @@
 using System;
 using System.IO;
 using System.Collections.Generic;
+using grocerystore;
 
-class kiosk {         
-    static void Main(string[] args)
-    {
-        //program takes the catalogue file name as a command line argument
-        if (args.Length == 0 || !File.Exists(args[0])){
-            System.Console.WriteLine("please enter a valid pricing catalogue.");
-            return;
-        }
-
-        bool on = true;
-        while(on){
-            //get the name of the basket file from the user
-            System.Console.WriteLine("enter the name of the basket file or type 'q' to exit");
-            string cartFile = Console.ReadLine();
-
-            //allow the user to exit if they choose
-            if(cartFile == "q"){
+namespace grocerystore
+{
+    class kiosk {         
+        static void Main(string[] args)
+        {
+            //program takes the catalogue file name as a command line argument
+            if (args.Length == 0 || !File.Exists(args[0])){
+                System.Console.WriteLine("please enter a valid pricing catalogue.");
                 return;
             }
 
-            //check the user entered a valid file
-            if (!File.Exists(cartFile)){
-                System.Console.WriteLine("invalid file name.");
-                continue;
-            }     
+            while(true){
+                //get the name of the basket file from the user
+                System.Console.WriteLine("enter the name of the basket file or type 'q' to exit");
+                string cartFile = Console.ReadLine();
 
-            checkout(cartFile, args[0]);       
+                //allow the user to exit if they choose
+                if(cartFile == "q"){
+                    return;
+                }
 
-        }
+                //check the user entered a valid file
+                if (!File.Exists(cartFile)){
+                    System.Console.WriteLine("invalid file name.");
+                    continue;
+                }     
 
-    }
+                checkout(cartFile, args[0]);       
 
-    static void checkout(string cart, string catalogue){
-        //read in the content of the cart file
-        string[] items = File.ReadAllLines(cart);  
-        string[] prices = File.ReadAllLines(catalogue);
-
-        //create a dictionary of the items and their prices 
-        IDictionary<string,float> catalogueDict = new Dictionary<string,float>();
-        foreach (string price in prices){
-            string[] catalogueInfo = price.Split(',');
-            string foodItem = catalogueInfo[0];
-            float itemPrice = float.Parse(catalogueInfo[1]);
-            catalogueDict.Add(foodItem, itemPrice);
-
-            //check if the item is on sale and add it to the dict
-            if(catalogueInfo.Length > 2){
-                float salePrice = float.Parse(catalogueInfo[2]);
-                foodItem = foodItem + "sale";
-                catalogueDict.Add(foodItem, salePrice);
-            }
-        }
-
-        //find the item prices
-        float totalPrice = 0;
-        string recieptString = "";
-        foreach (string item in items){
-
-            if(!catalogueDict.ContainsKey(item)){
-                //not sure how this should be handles so I just printed an error message
-                System.Console.WriteLine(item + " not found in the catalogue. I guess its free!!");
-                continue;
-            }
-
-            if(catalogueDict.ContainsKey(item + "sale")){
-                //item is on sale use this price instead
-                totalPrice = totalPrice + catalogueDict[item + "sale"];
-                recieptString = recieptString + item + " = normally: " + catalogueDict[item].ToString() + " now: " + catalogueDict[item + "sale"].ToString() + "\n";
-                continue;
-            }
-            else{
-                //item isnt on sale, use normal price
-                totalPrice = totalPrice + catalogueDict[item];
-                recieptString = recieptString + item + " = " + catalogueDict[item].ToString() + "\n";
             }
 
         }
-        recieptString = recieptString + "\nTotal = " + totalPrice.ToString() + "\nThanks for shopping with us!";  
-        System.Console.WriteLine(recieptString);
+
+        static void checkout(string cart, string catalogue){
+
+            //read in the content of the catalogue file  
+            string[] prices = File.ReadAllLines(catalogue);
+
+            //create a list of the items and their prices 
+            List<foodItem> catalogueList = new List<foodItem>();
+            foreach (string price in prices){
+                string[] catalogueInfo = price.Split(',');
+                string itemName = catalogueInfo[0];
+                string itemPrice = catalogueInfo[1];
+
+                //check if the item is on sale and add it to the list
+                if(catalogueInfo.Length > 2){
+                    string salePrice = catalogueInfo[2];
+                    grocerystore.foodItem foodObject = new foodItem(itemName, itemPrice, salePrice);
+                    catalogueList.Add(foodObject);
+                    continue;
+                }
+
+                //if the item isnt on sale add it to the list
+                grocerystore.foodItem foodObj = new foodItem(itemName, itemPrice);
+                catalogueList.Add(foodObj);
+
+            }
+
+            //read in the content of the cart file
+            string[] items = File.ReadAllLines(cart);  
+
+            //find the item prices
+            float totalPrice = 0;
+            string receiptString = "";
+            foreach (string item in items){
+                //find the index of the object correlating to the food item
+                int index = catalogueList.FindIndex(food => food.getItemName() == item);
+
+                //check if that item is in the catalogue
+                if(!(index >= 0))
+                {
+                    //not sure how this should be handled so I just printed an error message
+                    System.Console.WriteLine(item + " not found in the catalogue. I guess its free!!");
+                    continue;
+                }
+                else
+                {
+                    var food = catalogueList[index];
+                    //check if there is a sale on the item
+                    if (food.getSalePrice() < 0){
+                        //item isnt on sale
+                        receiptString += food.getItemName() + " = $" + food.getItemPrice().ToString("0.00") + "\n";
+                        totalPrice += food.getItemPrice();
+                    }
+                    else{
+                        //item is on sale
+                        receiptString += food.getItemName() + " = Regular Price: $" + food.getItemPrice().ToString("0.00") + " Sale Price: $" + food.getSalePrice().ToString("0.00") + "\n"; 
+                        totalPrice +=food.getSalePrice();
+                    }
+                }
+            }
+            //print final results
+            receiptString = receiptString + "\nTotal = $" + totalPrice.ToString("0.00") + "\nThanks for shopping with us!\n\n";  
+            System.Console.WriteLine(receiptString);
+        }
     }
 }
